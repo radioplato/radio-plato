ed01019a11f6976e061877a2f35e18a5133cad59
pavel-kirpikau <pavel.kirpikau@gmail.com>
2020-04-16 21:54:52 +0300

    initial site header cleanup version

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Icon} from '@iconify/react';
+import {Icon} from '@iconify/react';
 import mixcloudIcon from '@iconify/icons-cib/mixcloud';
 import facebookIcon from '@iconify/icons-cib/facebook';
 import vkIcon from '@iconify/icons-cib/vk';
@@ -8,16 +8,14 @@ import instagramIcon from '@iconify/icons-cib/instagram';
 import telegramIcon from '@iconify/icons-cib/telegram';
 import googlePlay from '@iconify/icons-cib/google-play';
 import bxSearch from '@iconify/icons-bx/bx-search';
-
 import './Header.css'
-
 
 
 function Header() {
     return (
-      <div className='.header flex-container'>
-        <p className="logo-text flex-item:nth-child(1)">RADIO PLATO</p>
-        <div className='social-icons flex-item:nth-child(2)'>
+      <div className='header header-container'>
+        <p className="logo-text">RADIO PLATO</p>
+        <div className='social-icons'>
             <a target="_blank" href="https://www.mixcloud.com/radioplato/" rel="noopener noreferrer"><Icon className='social-icon' icon={mixcloudIcon} width='2em' color='white'/></a>
             <a target="_blank" href="https://www.facebook.com/radioplato.by/" rel="noopener noreferrer"><Icon className='social-icon' icon={facebookIcon} width='1.8em' color='white'/></a>
             <a target="_blank" href="https://vk.com/radioplato" rel="noopener noreferrer"> <Icon className='social-icon' icon={vkIcon} width='1.8em' color='white'/></a>
@@ -26,8 +24,8 @@ function Header() {
             <a target="_blank" href="https://t.me/radioplato" rel="noopener noreferrer"> <Icon className='social-icon' icon={telegramIcon} width='1.8em' color='white'/></a>
             <a target="_blank" href="https://play.google.com/store/apps/details?id=com.radioplato" rel="noopener noreferrer"> <Icon className='social-icon' icon={googlePlay} width='1.8em' color='white'/></a>
         </div>
-        <p className="donate flex-item:nth-child(3)">DONATE</p>
-        <div className='search-icon flex-item:nth-child(4)'>
+        <p className="donate">DONATE</p>
+        <div className='search-icon'>
             <Icon icon={bxSearch} width='1.8em' color='white'/>
             </div>
       </div>
