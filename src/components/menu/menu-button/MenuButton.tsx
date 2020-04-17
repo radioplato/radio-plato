429b0f633fd9492f69c2754e913b6b85ce7e9eea
GitStearis <egor.puisha@gmail.com>
2020-04-17 22:12:32 +0300

    Add sidemenu component, update layout

---
+++ src/components/menu/menu-button/MenuButton.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import Menu from '../Menu';
+
+import './MenuButton.css'
+
+
+const MENU = "menu";
+
+function MenuButton() {
+    const menuRef = React.createRef<HTMLDivElement>()
+    
+    const toggleMenu = () => {
+        menuRef.current?.classList.toggle('menu-open');
+    }
+
+    return (
+        <>
+            <Menu menuRef={ menuRef } />
+            <div className="menuButton" onClick={ toggleMenu }>
+                <p className="label">{ MENU }</p>
+            </div>
+        </>
+    );
+  }
+  
+  export default MenuButton
