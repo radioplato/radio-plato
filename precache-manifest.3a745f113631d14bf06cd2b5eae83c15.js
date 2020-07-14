fea1b8568669fe5c97ca81e58e0db80411db2351
GitStearis <egor.puisha@gmail.com>
2020-07-14 11:20:53 +0300

    Updates

---
+++ precache-manifest.3a745f113631d14bf06cd2b5eae83c15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "737d3d452c3b4564c346fd4f6ee04fd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "210cb3b8026541421c66",
+    "url": "/static/css/2.c7661d7b.chunk.css"
+  },
+  {
+    "revision": "3ddda146404f127888d1",
+    "url": "/static/css/main.4a781af5.chunk.css"
+  },
+  {
+    "revision": "210cb3b8026541421c66",
+    "url": "/static/js/2.c0d22f06.chunk.js"
+  },
+  {
+    "revision": "52769c894fc160ba73e9c60bc8ff100f",
+    "url": "/static/js/2.c0d22f06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ddda146404f127888d1",
+    "url": "/static/js/main.e63fdd4e.chunk.js"
+  },
+  {
+    "revision": "26f5dd75053a0d121d50",
+    "url": "/static/js/runtime-main.fae0baa0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/radio-plato/precache-manifest.6c53ea6d4e6e47f0e0b460060dced621.js"
+  "/precache-manifest.3a745f113631d14bf06cd2b5eae83c15.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/radio-plato/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
