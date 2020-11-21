7f82f42afa262efaa93b184e109f401a5e6e5e2e
pavel-kirpikau <pavel.kirpikau@gmail.com>
2020-11-21 16:42:40 +0300

    google play link separated x minor css fixes

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -15,10 +15,13 @@ import { SocialLinks } from '../shared/interfaces';
 import { PlayerTypes } from '../shared/enums';
 
 import './Header.css'
+import Icon from '@iconify/react';
+import googlePlay from '@iconify/icons-cib/google-play';
 
 
 const RADIO_PLATO = 'Radio Plato';
 const DONATE = 'Donate';
+const ANDROID_APP = 'Android App';
 
 const HEADER_SOCIAL_LINKS: SocialLinks = {
     mixcloud: 'https://www.mixcloud.com/radioplato/',
@@ -47,6 +50,18 @@ function Header() {
                             props => (<PlayerComponent { ...props } playerType={ PlayerTypes.Header }/>)
                         }/>
                     </Switch>
+                    
+                    <a  className='android-link'
+                        target='_blank'
+                        href={ HEADER_SOCIAL_LINKS.googlePlay }
+                        title={ `A link to Android App` }
+                        aria-label={ `A link to Android App` }
+                        rel='noopener noreferrer'
+                    >
+                        <Icon className='social-icon' icon={ googlePlay  } width='1.5em' color='white'/>
+                        { ANDROID_APP }
+                    </a>
+
                     <Link to='/donate' className='donate-link'>{ DONATE }</Link>
                 </div>
             </BrowserView>
