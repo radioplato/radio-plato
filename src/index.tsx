56c7e696fd000e64d876015a31285e73916f2bfe
George Puisha <egor.puisha@gmail.com>
2020-11-25 00:55:31 +0300

    Prerender (#26)
    * Prerenderer
    
    * fixup

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
+
+const rootElement = document.getElementById("root");
+
+if (rootElement && rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  rootElement);
+} else {
+  ReactDOM.render(<React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement);
+}
