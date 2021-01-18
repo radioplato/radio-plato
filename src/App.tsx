cf8386c7ceeed7531e0e8b81316010810ef71bff
GitStearis <egor.puisha@gmail.com>
2021-01-18 19:18:54 +0300

    Add GA

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import {
     BrowserRouter, HashRouter
 } from 'react-router-dom';
-
+import Analytics from 'react-router-ga';
 import { HelmetProvider } from 'react-helmet-async';
 
 import Header from './components/header/Header';
@@ -21,18 +21,27 @@ const Components = (
     </>
 );
 
+function withAnalytics (children: JSX.Element) {
+    return navigator.userAgent !== "ReactSnap" ?
+    (
+        <Analytics id={ process.env.REACT_APP_GA }>
+            { children }
+        </Analytics>
+    ) : children;
+}
+
 function withEnvironmentalRouter (children: JSX.Element) {
     return process.env.REACT_APP_ENV === 'production' ?
     (
         <BrowserRouter>
-            { children }
+            { withAnalytics(children) }
         </BrowserRouter>
     ) :
     (
         <HashRouter basename='/'>
             { children }
         </HashRouter>
-    )
+    );
 }
 
 class App extends Component {
