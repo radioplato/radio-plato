2cee5cd23eed5371c11e6517031c65e49e469c19
pavel-kirpikau <pavel.kirpikau@gmail.com>
2022-11-20 14:16:51 +0200

    add iOS app privacy policy

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -15,6 +15,7 @@ import { SchedulePageComponent } from '../shared/schedule/ScheduleComponent';
 import DonateComponent from '../donate/Donate'
 import StudioComponent from '../studio/Studio';
 import { NotFoundComponent } from '../not-found/NotFoundComponent';
+import iOSppComponent from '../ios-privacy-policy/ios-privacy-policy';
 
 import { NewsListTypes } from '../shared/enums';
 
@@ -28,6 +29,7 @@ enum ROUTES {
     ABOUT = '/about',
     DONATE = '/donate',
     STUDIO = '/studio',
+    IOS = '/radio-plato-i-os-app-privacy-policy'
 }
 
 function Content() {
@@ -41,6 +43,7 @@ function Content() {
             <Route exact path={ `${ ROUTES.NEWS }/:category` } component={ (routerProps: any) => <NewsListComponent type={ NewsListTypes.Full } category={ routerProps.match.params.category } /> } />
             <Route exact path={ `${ ROUTES.NEWS }/:category/:slug` } component={(routerProps: any) => <NewsComponent slug={routerProps.match.params.slug} />} />
             <Route exact path={ ROUTES.ABOUT } component={ AboutComponent } />
+            <Route exact path={ ROUTES.IOS } component={ iOSppComponent } />
             <Route exact path={ ROUTES.DONATE } component={ DonateComponent } />
             <Route exact path={ ROUTES.STUDIO } component={ StudioComponent } />
             <Route component={ NotFoundComponent } />
---
+++ src/components/ios-privacy-policy/interfaces.ts
@@ -0,0 +1,12 @@
+import { StrapiObjectDto } from '../shared/interfaces';
+
+export interface iOSppDto extends StrapiObjectDto {
+    text: string;
+    tlug: string;
+    title: string;
+}
+
+export interface iOSpp {
+    text: string;
+    title: string;
+}
