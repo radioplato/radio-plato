e1c2abfa48050526c84ab5822084dfb4fa86bc15
GitStearis <egor.puisha@gmail.com>
2023-04-20 22:27:24 +0300

    Add a heart to the menu 'Donate' link

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -4,7 +4,11 @@ import { Link } from 'react-router-dom';
 
 import { isMobileOnly } from 'react-device-detect';
 
+import Icon from '@iconify/react';
+import cardsHeart from '@iconify/icons-mdi/cards-heart';
+
 import { DONATE_LINK } from '../shared/constants';
+
 import './Menu.css'
 
 
@@ -43,6 +47,7 @@ function Menu({ menuRef, wrapperRef, toggleMenu }: MenuParameters) {
                 <div className='menu-items-container'>
                     <ul className='menu-items'>
                         { PAGES.map(title => buildMenuItem(title)) }
+                        <div className='menu-separator'></div>
                         <li className='menu-item' key='donate'>
                             <a
                                 target='_blank'
@@ -50,7 +55,10 @@ function Menu({ menuRef, wrapperRef, toggleMenu }: MenuParameters) {
                                 title={ `A Radio Plato Patreon link` }
                                 aria-label={ `A Radio Plato Patreon link` }
                                 rel='noopener noreferrer'
-                            >Donate</a>
+                            >
+                                Donate
+                                <Icon className='heart-icon' icon={ cardsHeart } width='32' height='32'/>
+                            </a>
                         </li>
                     </ul>
                 </div>
