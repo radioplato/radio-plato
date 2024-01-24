db624e9b2fbf6c36a449303d9d349c402f104182
findscode <egor.puisha@gmail.com>
2024-01-24 19:08:49 +0300

    Some more mobile player enhancements

--- src/components/shared/player/components/footer-player/FooterPlayerComponent.tsx
+++ src/components/shared/player/components/footer-player/FooterPlayerComponent.tsx
@@ -20,7 +20,7 @@ import './FooterPlayerComponent.scss';
 
 function FooterPlayerComponent() {
     const location = useLocation();
-    
+
     const [schedule, setSchedule] = useState<ScheduleCard[][]>(scheduleService.schedule);
     const [scheduleCard, setScheduleCard] = useState<ScheduleCard | undefined>();
     const [playing, setPlaying] = useState(playerService.playing);
@@ -61,7 +61,7 @@ function FooterPlayerComponent() {
 
         setScheduleCard(schedule[weekday].find((scheduleCard) =>
             scheduleCard.azuracastID === nowPlayingInformation?.name
-                && moment().isBetween(moment(scheduleCard.startDate), moment(scheduleCard.endDate))
+            && moment().isBetween(moment(scheduleCard.startDate), moment(scheduleCard.endDate))
         ));
     }, [schedule, nowPlayingInformation]);
 
@@ -76,34 +76,48 @@ function FooterPlayerComponent() {
     }
 
     const togglePanel = () => {
+        document.querySelector('.header-container.mobile')?.classList.toggle('bring-back');
         panelRef.current?.classList.toggle('full');
     }
 
     return (
         <div className='player'>
-            <div ref={panelRef} className='main-panel'>
-                <div className='controls-container'>
+            <div ref={panelRef} className='main-panel' style={{
+                backgroundImage: `url(${trackArt})`,
+            }}>
+                <div className='content-container'>
+                    <div className='controls-container'>
+                        <Button
+                            className='fullscreen-button'
+                            type={BUTTON_TYPE.GHOST}
+                            size={BUTTON_SIZE.LARGE}
+                            icon={ICON_KEY.CHEVRON_REGULAR}
+                            iconRotate={180}
+                            title='minimize player'
+                            onClick={togglePanel}
+                        />
+                    </div>
+                    <div className='track-art-container'>
+                        <div
+                            className='track-art'
+                            style={{
+                                backgroundImage: `url(${trackArt})`,
+                            }}
+                            onClick={togglePanel}
+                        >
+                        </div>
+                    </div>
+                    <TrackTitle className='track-title-component' isTicker={false} showOnAir={false} />
                     <Button
-                        className='fullscreen-button'
-                        type={BUTTON_TYPE.GHOST}
-                        size={BUTTON_SIZE.LARGE}
-                        icon={ICON_KEY.CHEVRON_REGULAR}
-                        iconRotate={180}
-                        title='minimize player'
-                        onClick={togglePanel}
+                        className='play-button'
+                        type={BUTTON_TYPE.OUTLINE}
+                        size={BUTTON_SIZE.ENORMOUS}
+                        icon={playing ? ICON_KEY.PAUSE_FILLED : ICON_KEY.PLAY_FILLED}
+                        title={playing ? 'pause' : 'play'}
+                        onClick={togglePlayingMode}
                     />
+                    <ScheduleLine card={scheduleCard} isNow={true} />
                 </div>
-                <div className='track-art-container'>
-                    <div
-                        className='track-art'
-                        style={{
-                            backgroundImage: `url(${trackArt})`,
-                        }}
-                        onClick={togglePanel}
-                    ></div>
-                </div>
-                <TrackTitle className='track-title-component' isTicker={false} showOnAir={false}/>
-                <ScheduleLine card={scheduleCard} isNow={true} />
             </div>
             <div className='footer-panel'>
                 <div
@@ -113,7 +127,7 @@ function FooterPlayerComponent() {
                     }}
                     onClick={togglePanel}
                 ></div>
-                <TrackTitle className='track-title-component' isTicker={true} showOnAir={true}/>
+                <TrackTitle className='track-title-component' isTicker={true} showOnAir={true} />
                 <Button
                     className='play-button'
                     type={BUTTON_TYPE.OUTLINE}
