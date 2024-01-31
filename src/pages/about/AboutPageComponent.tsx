1cd5cdbd6bd084c38eac0e981fe8a3bcde8a62b2
findscode <egor.puisha@gmail.com>
2024-02-01 02:43:50 +0300

    'About' page updates: markdown, donate button

--- src/pages/about/AboutPageComponent.tsx
+++ src/pages/about/AboutPageComponent.tsx
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from 'react';
 
+import ReactMarkdown from 'react-markdown';
 import qs from 'qs';
 
 import { Seo } from '../../components/shared/wrappers/seo/Seo'
 import SocialButtonsComponent from '../../components/shared/social-links/components/social-buttons/SocialButtonsComponent';
+import { BUTTON_SIZE, BUTTON_TYPE, Button, ICON_POSITION } from '../../components/shared/button/components/Button';
 
 import { AboutPageContent, AboutPageDto } from './models';
 import { PLATO_SOCIAL_BUTTONS } from '../../components/shared/social-links/constants';
+import { ICON_KEY } from '../../components/shared/icons/icons';
+import { DONATE_LINK } from '../../components/shared/constants';
 
 import './AboutPageComponent.scss';
 
 export function AboutPageComponent() {
     const [about, setAbout] = useState<AboutPageContent | null>(null);
-    
+
     const parseAbout = (aboutDto: AboutPageDto): AboutPageContent | null => {
         return aboutDto ? {
             title: aboutDto.data.attributes.Title,
@@ -29,7 +33,7 @@ export function AboutPageComponent() {
         const query = qs.stringify({
             populate: '*',
         });
-        
+
         fetch(`${process.env.REACT_APP_BACKEND_URL_V2}/about?${query}`)
             .then(response => response.json())
             .then((data: AboutPageDto) => parseAbout(data))
@@ -39,7 +43,7 @@ export function AboutPageComponent() {
     useEffect(() => {
         loadAboutPage();
     }, []);
-    
+
     return about ? (
         <article className='about-container'>
             <Seo meta={{
@@ -51,10 +55,27 @@ export function AboutPageComponent() {
             <div className='about'>
                 <div className='information'>
                     <h1 className='about-title'>{about.title}</h1>
-                    <div className='about-text'>{about.description}</div>
+                    <div className='about-text'>
+                        <ReactMarkdown
+                            source={about.description}
+                            escapeHtml={navigator.userAgent === 'ReactSnap'}
+                            renderers={{
+                                link: props => <a href={props.href} target="_blank" rel="noopener noreferrer">{props.children}</a>
+                            }}
+                        />
+                    </div>
                     <div className='social-buttons'>
                         <SocialButtonsComponent socialLinks={PLATO_SOCIAL_BUTTONS}></SocialButtonsComponent>
                     </div>
+                    <Button
+                        type={BUTTON_TYPE.OUTLINE}
+                        size={BUTTON_SIZE.MEDIUM}
+                        icon={ICON_KEY.HEART_REGULAR}
+                        iconPosition={ICON_POSITION.LEFT}
+                        label='donate'
+                        href={DONATE_LINK}
+                        title='donate link'
+                    ></Button>
                 </div>
                 <div className='image'>
                     <img src={about.aboutCover.url} loading='lazy' alt={about.aboutCover.alternativeText} />
